f97a7d70e77cacb84ed25b067cdcd7f6cdc52304
funnytony <tonsloth@gmal.com>
2018-11-12 08:20:48 +0300

    Template

---
+++ WebShop/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }//код товара
+
+        public string Name { get; set; }//название товара
+
+        public string Description { get; set; }//описание товара
+
+        public string FullDescription { get; set; }//подробное описание товара
+
+        public string Appearance { get; set; }//внешний вид
+
+        public int Price { get; set; }//цена
+
+        public bool Sale { get; set; }//распродажа
+
+        public bool New { get; set; }//новые поступления
+
+        public string Image { get; set; }//изображение
+
+
+    }
+}
---
+++ WebShop/SampleData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Models;
+
+namespace WebShop
+{
+    public static class SampleData
+    {
+        public static void Initialize(ProductContext context)
+        {
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {                        
+                        Name = "Ягодный торт",
+                        Description = "Торт с ягодами на день рождения",
+                        FullDescription = "Внутри сочный красный бархат, кремчиз и прослойка из свежей клубники",
+                        Appearance = "Торт оформлен свежими ягодами по краю. В центре надпись с поздравлением",
+                        Price = 20,
+                        Sale = false,
+                        New = false,
+                        Image = "tort.jpg"
+                    },
+                    new Product
+                    {
+                        Name = "Пряники-снежинки",
+                        Description = "Имбирные пряники",
+                        FullDescription = "Нежные имбирные пряники с сахарной глазурью",
+                        Appearance = "Пряники в виде снежинок подаются в коробке на бумажной подложке",
+                        Price = 30,
+                        Sale = true,
+                        New = false,
+                        Image = "pryaniki.jpg"
+                    },
+                    new Product
+                    {
+                        Name = "Капкейки",
+                        Description = "Сладкие капкейки",
+                        FullDescription = "Шоколадные с апельсиновым курдом и ванильные со сливочной смородиной",
+                        Appearance = "Оформлены кусочками апельсина с шоколадом и мятой, а так же венскими вафлями",
+                        Price = 40,
+                        Sale = false,
+                        New = true,
+                        Image = "cupcake.jpg"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
